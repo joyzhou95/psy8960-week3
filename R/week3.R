47ea4a1e1b75f6c9e68b5a2ce69285a22e932c21
Joy Zhou <zhou1559@umn.edu>
2023-02-02 15:44:14 -0600

    Create barplots for all items

--- R/week3.R
+++ R/week3.R
@@ -12,3 +12,4 @@ clean_df <- clean_df[clean_df$q6 == 1,]
 clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units = "mins")
 hist(as.numeric(clean_df$timeSpent))
 frequency_tables_list <- lapply(clean_df[,5:14], table)
+lapply(frequency_tables_list, barplot)
