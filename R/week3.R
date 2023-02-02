18dd39fa056cba83837891f6ae7bcc5f7f5823b7
Joy Zhou <zhou1559@umn.edu>
2023-02-02 15:44:49 -0600

    Count the # of obs that meet the selection criteria

--- R/week3.R
+++ R/week3.R
@@ -13,3 +13,4 @@ clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units = "mi
 hist(as.numeric(clean_df$timeSpent))
 frequency_tables_list <- lapply(clean_df[,5:14], table)
 lapply(frequency_tables_list, barplot)
+sum(clean_df$q1 >= clean_df$q2 & clean_df$q2 != clean_df$q3)
