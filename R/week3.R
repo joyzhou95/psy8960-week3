87d3a803cf0f3a6bc3c1ca404bbe9774e380a9cf
Joy Zhou <zhou1559@umn.edu>
2023-02-02 15:37:04 -0600

    Create the variable of timeSpent

--- R/week3.R
+++ R/week3.R
@@ -7,3 +7,6 @@ raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
 clean_df <- raw_df[raw_df$timeEnd > "2017-07-01 00:00:00",]
 clean_df <- clean_df[clean_df$q6 == 1,]
+
+#Analysis
+clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units = "mins")
