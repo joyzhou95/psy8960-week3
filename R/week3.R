050e3a61c922bc03b38f7f09a63ef00e62ee93e0
Joy Zhou <zhou1559@umn.edu>
2023-02-02 15:35:55 -0600

    Exclude research assistants

--- R/week3.R
+++ R/week3.R
@@ -5,3 +5,5 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 raw_df <- read.csv(file = "../Data/week3.csv", header = T)
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+clean_df <- raw_df[raw_df$timeEnd > "2017-07-01 00:00:00",]
+
