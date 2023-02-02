4d9df95089c7d721cb9b968322ae8daa9c662994
Joy Zhou <zhou1559@umn.edu>
2023-02-02 15:35:25 -0600

    Convert the format of time variables

--- R/week3.R
+++ R/week3.R
@@ -3,3 +3,5 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 #Data Import and Cleaning
 raw_df <- read.csv(file = "../Data/week3.csv", header = T)
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
