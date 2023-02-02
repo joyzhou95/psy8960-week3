c65f4c41aa30ea66e8f33f975592b750f80a4b7f
Joy Zhou <zhou1559@umn.edu>
2023-02-02 15:43:40 -0600

    Create frequency tables for item responses

--- R/week3.R
+++ R/week3.R
@@ -11,3 +11,4 @@ clean_df <- clean_df[clean_df$q6 == 1,]
 #Analysis
 clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units = "mins")
 hist(as.numeric(clean_df$timeSpent))
+frequency_tables_list <- lapply(clean_df[,5:14], table)
