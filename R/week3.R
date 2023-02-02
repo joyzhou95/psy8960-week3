9069356269c7f71796712a9b76fdbf885a301089
Joy Zhou <zhou1559@umn.edu>
2023-02-02 15:36:22 -0600

    Exclude respondents didn't pay attention

--- R/week3.R
+++ R/week3.R
@@ -6,4 +6,4 @@ raw_df <- read.csv(file = "../Data/week3.csv", header = T)
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
 clean_df <- raw_df[raw_df$timeEnd > "2017-07-01 00:00:00",]
-
+clean_df <- clean_df[clean_df$q6 == 1,]
